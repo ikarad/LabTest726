f49f426a7cd13107be1bf5d5ef7a224d9dd3b548
Nonnapat <87961234+ikarad@users.noreply.github.com>
2024-11-10 15:54:47 +0700

    Update app_predict_knn_penguin.py

--- app_predict_knn_penguin.py
+++ app_predict_knn_penguin.py
@@ -36,8 +36,7 @@ st.subheader('Penguin Species Prediction')
 if st.button('Predict'):
     # ทำนายผล
     prediction = model.predict(user_input)
-    result = species_encoder.inverse_transform(prediction)
 
    # แสดงผลการทำนาย
     st.subheader('Prediction Result')
-    st.write(f"The predicted species is: {result}")
+    st.write(f"The predicted species is: {prediction}")
