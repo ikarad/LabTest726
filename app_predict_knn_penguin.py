8952a245382733ac9e4bf1834ff8b489615ba950
Nonnapat <87961234+ikarad@users.noreply.github.com>
2024-11-10 14:24:02 +0700

    Update app_predict_knn_penguin.py

--- app_predict_knn_penguin.py
+++ app_predict_knn_penguin.py
@@ -20,6 +20,15 @@ user_input = np.array([culmen_length, culmen_depth, flipper_length, body_mass, i
 # แปลงข้อมูลของผู้ใช้
 user_input[:, 4] = island_encoder.transform(user_input[:, 4])  # island encoding
 user_input[:, 5] = sex_encoder.transform(user_input[:, 5])  # sex encoding
+x_new =  pd.DataFrame() 
+x_new['island'] = [island]
+x_new['culmen_length_mm'] = [culmen_length]
+x_new['culmen_depth_mm'] = [culmen_depth]
+x_new['flipper_length_mm'] = [flipper_length]
+x_new['body_mass_g'] = [body_mass]
+x_new['sex'] = [sex]
+x_new['island'] = island_encoder.transform(x_new['island'])
+x_new['sex'] = sex_encoder.transform(x_new['sex'])
 
 # ทำนายผล
 prediction = model.predict(user_input)
