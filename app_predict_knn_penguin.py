630a5613cdb4170f652d2b13c9ad1441bf46081a
Nonnapat <87961234+ikarad@users.noreply.github.com>
2024-11-10 14:20:17 +0700

    Update app_predict_knn_penguin.py

--- app_predict_knn_penguin.py
+++ app_predict_knn_penguin.py
@@ -7,10 +7,10 @@ with open('model_penguin_726.pkl', 'rb') as file:
     model, species_encoder, island_encoder, sex_encoder = pickle.load(file)
 
 # รับค่าจากผู้ใช้
-culmen_length = st.sidebar.number_input('Culmen Length (mm)', min_value=0.0, max_value=100.0, value=40.0)
-culmen_depth = st.sidebar.number_input('Culmen Depth (mm)', min_value=0.0, max_value=100.0, value=20.0)
-flipper_length = st.sidebar.number_input('Flipper Length (mm)', min_value=0.0, max_value=100.0, value=190.0)
-body_mass = st.sidebar.number_input('Body Mass (g)', min_value=0.0, max_value=10000.0, value=3500.0)
+culmen_length = st.sidebar.number_input('Culmen Length (mm)', value=40.0)
+culmen_depth = st.sidebar.number_input('Culmen Depth (mm)', value=20.0)
+flipper_length = st.sidebar.number_input('Flipper Length (mm)', value=190.0)
+body_mass = st.sidebar.number_input('Body Mass (g)', value=3500.0)
 island = st.sidebar.selectbox('Island', options=['Torgersen', 'Biscoe', 'Dream'])
 sex = st.sidebar.selectbox('Sex', options=['MALE', 'FEMALE'])
 
